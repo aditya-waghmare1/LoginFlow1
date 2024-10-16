9bddf31a467c7287c409481e336de4b1abf1115e
Aditya-waghmare <106002614+aditya-waghmare1@users.noreply.github.com>
2024-10-16 12:20:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';
+import HomeScreen from './HomeScreen';
+import ForgotPasswordScreen from './ForgotPasswordScreen';
+import SetNewPasswordScreen from './SetNewPasswordScreen';
+import SignUpScreen from './SignUpScreen'; // Import the SignUpScreen
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="SignUp" 
+          component={SignUpScreen} // Add the SignUpScreen here
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="ForgotPassword" 
+          component={ForgotPasswordScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="SetNewPassword" 
+          component={SetNewPasswordScreen} 
+          options={{ headerShown: false }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
